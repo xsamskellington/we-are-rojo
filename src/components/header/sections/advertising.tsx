269d61e428a0c69afd83e08beeabcb6a705bf7f7
xsamskellington <samantha.valdez.toccalino@gmail.com>
2023-11-21 00:05:52 -0300

    JSDOM

--- src/components/header/sections/advertising.tsx
+++ src/components/header/sections/advertising.tsx
@@ -7,12 +7,16 @@ import { advertising } from '../data';
 const Advertising = () => {
   const [openAdvertising, setOpenAdvertising] = useState(false);
   const advertisingMenu = useRef<HTMLDivElement>(null);
+  const jsdom = require('jsdom');
+  const { JSDOM } = jsdom;
+
+  const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+  const document = dom.window.document;
 
   const closeAdvertising = (e: any) => {
     if (
       advertisingMenu.current &&
       openAdvertising &&
-      advertisingMenu.current instanceof HTMLDivElement &&
       !advertisingMenu.current.contains(e.target)
     ) {
       setOpenAdvertising(false);
