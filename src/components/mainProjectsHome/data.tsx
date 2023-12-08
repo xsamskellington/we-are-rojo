331f8545307a9f9b938dfb3293188e806896b5f1
xsamskellington <samantha.valdez.toccalino@gmail.com>
2023-12-08 18:59:18 -0300

    adding main projects

--- src/components/mainProjectsHome/data.tsx
+++ src/components/mainProjectsHome/data.tsx
@@ -1,31 +1,31 @@
 export const thumbnailProjects = [
   {
-    title: 'Ecos de un crimen',
+    title: 'ECOS DE UN CRIMEN',
     img: '/01_euc_home.png',
     href: '/features/ecos-de-un-crimen',
   },
   {
-    title: 'Misántropo',
+    title: 'MISANTROPO',
     img: '/02_misa_home.png',
     href: '/features/misantropo',
   },
   {
-    title: 'División Palermo',
+    title: 'DIVISION PALERMO',
     img: '/03_dp_home.png',
     href: '/episodic/division-palermo',
   },
   {
-    title: 'Los Impresentables',
+    title: 'LOS IMPRESENTABLES',
     img: '/04_impre_home.png',
     href: '/features/los-impresentables',
   },
   {
-    title: 'El Método Tangalanga',
+    title: 'EL METODO TANGALANGA',
     img: '/05_tanga_home.png',
     href: '/features/el-metodo-tangalanga',
   },
   {
-    title: 'Adiós, Buenos Aires',
+    title: 'ADIOS, BUENOS AIRES',
     img: '/06_chau_home.png',
     href: '/features/adios-buenos-aires',
   },
--- src/components/mainProjectsHome/index.tsx
+++ src/components/mainProjectsHome/index.tsx
@@ -1,29 +1,22 @@
 import './styles.css';
 import Image from 'next/image';
-import { thumbnailProjects } from '../../../public/data';
+import { thumbnailProjects } from './data';
 import Link from 'next/link';
 
 const MainProjectsHome = () => {
   return (
     <div className="thumbnail-container">
       {thumbnailProjects.map((project) => (
-        <div key={project.title} className="thumbnail">
-          <Link href={project.href}>
-            {/* <p>{project.title}</p> */}
-            <div
-              style={{ width: '45vw', height: '45vh', position: 'relative' }}
-            >
-              <Image
-                src={project.img}
-                alt="thumnbail"
-                layout="fill"
-                objectFit="contain"
-                // width={817}
-                // height={459}
-              />
-            </div>
-          </Link>
-        </div>
+        <Link
+          href={project.href}
+          className="thumbnail"
+          style={{ backgroundImage: `url(${project.img})` }}
+          key={project.title}
+        >
+          <div className="project-title">
+            <p>{project.title}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
