f4401a340e2bd04b5a0abefede39efc517e51480
xsamskellington <samantha.valdez.toccalino@gmail.com>
2024-02-13 19:21:30 -0300

    fixing error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100..700&display=swap"
           rel="stylesheet"
