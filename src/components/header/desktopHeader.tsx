6da0e31545f4f6525ba8f0882ae07caa7fab97c8
xsamskellington <samantha.valdez.toccalino@gmail.com>
2024-05-18 20:21:13 -0300

    adding commons and buttons

--- src/components/header/desktopHeader.tsx
+++ src/components/header/desktopHeader.tsx
@@ -3,6 +3,7 @@ import { menuSections } from './data';
 import Link from 'next/link';
 import Rojo from '../images/rojo.svg';
 import Image from 'next/image';
+import LanguageSwitch from './languageSwitch';
 
 const DesktopHeader = () => {
   const [selectedSection, setSelectedSection] = useState(null);
@@ -12,24 +13,28 @@ const DesktopHeader = () => {
   };
 
   return (
-    <div className="section-menu-desktop">
-      <a href={'/'} className="logo">
-        <Image src={Rojo} alt="rojo studio" width={80} height={50} />
-      </a>
-      {menuSections.map((section, index) => (
-        <>
-          <Link
-            href={section.href}
-            key={section.title}
-            className={`section ${selectedSection === index ? 'bold' : ''}`}
-            onClick={() => handleSectionClick(index)}
-          >
-            {section.title}
-          </Link>
-          {index < menuSections.length - 1 && '|'}
-        </>
-      ))}
-    </div>
+    <>
+      <div className="section-menu-desktop">
+        <a href={'/'} className="logo">
+          <Image src={Rojo} alt="rojo studio" width={80} height={50} />
+        </a>
+        {menuSections.map((section, index) => (
+          <>
+            <Link
+              href={section.href}
+              key={section.title}
+              className={`section ${selectedSection === index ? 'bold' : ''}`}
+              onClick={() => handleSectionClick(index)}
+            >
+              {section.title}
+            </Link>
+            {index < menuSections.length - 1 && '|'}
+          </>
+        ))}
+      </div>
+
+      <LanguageSwitch />
+    </>
   );
 };
 
---
+++ src/components/header/languageSwitch.tsx
@@ -0,0 +1,10 @@
+const LanguageSwitch = () => {
+  return (
+    <div className="language">
+      <button className="section">ES</button>/
+      <button className="section">EN</button>
+    </div>
+  );
+};
+
+export default LanguageSwitch;
