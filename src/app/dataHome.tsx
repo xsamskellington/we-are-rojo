13edf394b772d380282c56ce00649f42070e9c5c
xsamskellington <samantha.valdez.toccalino@gmail.com>
2024-05-22 11:43:28 -0300

    updating data title projects

--- src/app/dataHome.tsx
+++ src/app/dataHome.tsx
@@ -1,31 +1,37 @@
 export const projects = [
   {
-    title: 'JAQUE MATE',
+    titleEn: 'CHECKMATE',
+    titleEs: 'JAQUE MATE',
     href: '/features/jaque-mate',
     img: '/jaque-mate.jpg',
   },
   {
-    title: 'TO CATCH A KILLER',
+    titleEn: 'TO CATCH A KILLER',
+    titleEs: 'MISANTROPO',
     img: '/02-misantropo.jpg',
     href: '/features/misantropo',
   },
   {
-    title: 'CTRL + ALT + DESIRE',
+    titleEn: 'CTRL + ALT + DESIRE',
+    titleEs: 'CTRL + ALT + DESIRE',
     href: '/episodic/ctrl-alt-desire',
     img: '/ctrl-alt-desire.jpg',
   },
   {
-    title: 'ADIOS, BUENOS AIRES',
+    titleEn: 'CHAU, BUENOS AIRES',
+    titleEs: 'ADIOS, BUENOS AIRES',
     img: '/04-adios-buenos-aires.jpg',
     href: '/features/adios-buenos-aires',
   },
   {
-    title: 'THE TANGALANGA METHOD',
+    titleEn: 'THE TANGALANGA METHOD',
+    titleEs: 'EL METODO TANGALANGA',
     img: '/05-tangalanga.jpg',
     href: '/features/el-metodo-tangalanga',
   },
   {
-    title: 'THE UNPRESENTABLES',
+    titleEn: 'THE UNPRESENTABLES',
+    titleEs: 'LOS IMPRESENTABLES',
     img: '/06-impresentables.jpg',
     href: '/features/los-impresentables',
   },
